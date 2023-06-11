2731e179c393503de6f7512974712d05c45d2331
nadikakpathirana <nadikakpathirana@gmail.com>
2023-06-11 08:30:39 +0530

    Add project files.

---
+++ MyFirstProjectWeb/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstProjectWeb.Data;
+using MyFirstProjectWeb.Models;
+
+namespace MyFirstProjectWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //get
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        //get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstProjectWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstProjectWeb.Models;
+
+namespace MyFirstProjectWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
